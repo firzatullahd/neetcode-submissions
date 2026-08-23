4df30f2e9a342d751f637690f37020fb3b108603
Firzatullah Dwiko Ramadhan <firzatullahdwikoramadhan@gmail.com>
2026-08-23 21:29:33 +0700

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+	var prev, current, next *ListNode
+
+	// todo add condition for break loop
+	current = head
+	for current != nil {
+		// save next node to a var, before overwrite
+		next = current.Next
+
+		// repoint current node to previous node
+		current.Next = prev
+
+		// save current node as previous (if last iteration, last node becomes prev)
+		prev = current
+
+		// focus to next node
+		current = next
+	}
+
+	return prev
+}
